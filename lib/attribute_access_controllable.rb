88ff2eb83552ec0115617af0f3a28ae5e00f60be
Ken Mayer <kmayer@bitwrangler.com>
2012-05-08 00:10:38 -0700

    Documentation update

--- lib/attribute_access_controllable.rb
+++ lib/attribute_access_controllable.rb
@@ -8,19 +8,18 @@ module AttributeAccessControllable
     validate :read_only_attributes_unchanged, :on => :update, :unless => :skip_read_only
   end
 
+  # Mark an attribute as read-only
+  # @param [Symbol] attributes on the model
+  # @return [void]
   def attr_read_only(*attributes)
     self.read_only_attributes = Set.new(attributes.map { |a| a.to_s }) + (read_only_attributes || [])
   end
 
+  # @param [Symbol] attribute on the model
+  # @return [Boolean] attribute is read-only
   def read_only_attribute?(attribute)
     return false if read_only_attributes.nil?
     read_only_attributes.member?(attribute.to_s)
-  end
-
-  def read_only_attributes_unchanged
-    changed_attributes.each_key do |attr|
-      errors.add(attr) << "is read_only" if read_only_attribute?(attr)
-    end
   end
 
   def save(options = {})
@@ -31,5 +30,13 @@ module AttributeAccessControllable
   def save!(options = {})
     self.skip_read_only = options.delete(:skip_read_only)
     super
+  end
+
+  private
+  
+  def read_only_attributes_unchanged
+    changed_attributes.each_key do |attr|
+      errors.add(attr) << "is read_only" if read_only_attribute?(attr)
+    end
   end
 end
